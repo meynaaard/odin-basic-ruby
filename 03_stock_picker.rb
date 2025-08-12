209503bc71254897657b6647e565033fc12c84ff
meynard <sombritomeynard@gmail.com>
2025-08-12 08:49:33 +0800

    refactor: Rename iterators of outer loop for clarity
    This change refactors the names of the outer loop
    iterators as each day is also a potential buy day

--- 03_stock_picker.rb
+++ 03_stock_picker.rb
@@ -5,17 +5,17 @@ My algorithm:
 1. Create a method #stock_picker that accepts an argument `stock_prices`, where
    `stock_prices` is an array of stock price per day.
 2. Iterate over the `stock_prices` array using #each_with_index, to get the
-   index as `day` starting from 0 and the `price` for each day
+   index as `buy_day` starting from 0 and the `buy_price` for each day
 3. Nest another iteration to check for potential `sell_day`, skip the day if it
-   is less than or equal the current `day`
-4. Calculate profit by subtracting the `price` from the `sell_price`
+   is less than or equal to `buy_day`
+4. Calculate profit by subtracting the `buy_price` from the `sell_price`
 =end
 
 def stock_picker(stock_prices)
-  stock_prices.each_with_index do |price, day|
+  stock_prices.each_with_index do |buy_price, buy_day|
     stock_prices.each_with_index do |sell_price, sell_day|
-      next if sell_day <= day
-      profit = sell_price - price
+      next if sell_day <= buy_day
+      profit = sell_price - buy_price
       binding.pry
     end
   end
