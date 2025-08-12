0067ac18514db19df647801f26d4fe5d7d4ce28b
meynard <sombritomeynard@gmail.com>
2025-08-12 11:15:02 +0800

    fix: Fix the Sub Strings Project requirements
    This change fixes the undesired behavior where the
    method only returns the tally that shows single
    occurrence of substrings.

--- 02_sub_strings.rb
+++ 02_sub_strings.rb
@@ -1,18 +1,27 @@
+require "pry-byebug"
+
 =begin
 My algorithm:
 1. Create a method #substrings that takes two arguments: `string` and
    `substring_list` where `string` is a string and `substring_list` is a
    valid array of substrings
-2. Iterate over the `substring_list` using #select and select the elements that
-   are substring of the provided `string` argument,
-3. Tally the selected using #tally elements in the `substring_list` to return
-   the hash of substring occurrence count
+2. Iterate over the `substring_list` using #reduce where initial value is an
+   empty hash, accumulator is `substring_tally` and current item is `substring`
+3. Use #downcase to `string` to make the check case-insensitive
+4. Chain #scan to get substring occurrence where argument is `substring`
+5. Chain #count to get count of the scanned occurrence
+6. Assign these chained methods to variable `substring_count`
+7. Add key/value pair by using `substring` as key and the `substring_count` as
+   value for the `substring_tally` hash
+8. Reject all pairs that have a value of 0, as they are irrelevant for the tally
 =end
 
 def substrings(string, substring_list)
-  substring_list.select do |substring|
-    string.downcase.include?(substring)
-  end.tally
+  substring_list.reduce({}) do |substring_tally, substring|
+    substring_count = string.downcase.scan(substring).count
+    substring_tally[substring] = substring_count
+    substring_tally.reject { |_, count| count == 0 }
+  end
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy",
