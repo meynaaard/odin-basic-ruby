1150d70a2c24f134d0a8582de05c9a3ad3b1c04e
meynard <sombritomeynard@gmail.com>
2025-08-13 07:27:39 +0800

    refactor: Refactor logic to use ASCII values instead of the character

--- 01_caesar_cipher.rb
+++ 01_caesar_cipher.rb
@@ -4,40 +4,40 @@ require "pry-byebug"
 My Algorithm:
 1. Create a method called #caesar_cipher that accepts two arguments: `string`
    and `key`
-2. Add a variable `lowercase_range` that includes range of lowercase letters
-3. Add a variable `uppercase_range` that includes range of uppercase letters
-4. Add a variable `split_string` that splits the `string` argument
-5. Create a new variable called shifted_values that #map over `split_string`
-   with iterator `char`
-6. Add a variable `shifted_value` that adds `key` to the `char` ordinal value
-7. IF `char` is covered within lowercase_range
-    shifted_value > 122('z'), subtract 26, otherwise return as is
-   ELSIF `char` is covered within uppercase_range
-    shifted_value > 90('Z'), subtract 26, otherwise return as is
+2. Add a variable `lowercase` that includes range of lowercase letters in ASCII
+3. Add a variable `uppercase` that includes range of uppercase letters in ASCII
+4. Add a variable `ascii_values` that splits the `string` argument then #map
+   into its character ASCII
+5. Create a new variable called `shifted_values` that #map over `ascii_values`
+   with iterator `code`
+6. Add a variable `shifted_value` that adds `key` to `code`
+7. IF code is covered within lowercase
+    shifted_value > lowercase end, subtract 26, otherwise return as is
+   ELSIF code is covered within uppercase
+    shifted_value > uppercase end, subtract 26, otherwise return as is
    ELSE
-    shifted_value is `char` ordinal value only (spaced, punctuations)
-8. Convert `shifted_values` back to a caesar cipher string using #chr and #join
+    return code
+8. #map shifted_values back to a caesar cipher string using #chr then #join
 =end
 
 def caesar_cipher(string, key)
-  lowercase_range = "a".."z"
-  uppercase_range = "A".."Z"
+  lowercase = 65..90  #=> a to z
+  uppercase = 97..122 #=> A to Z
 
-  split_string = string.split("")
+  ascii_values = string.split("").map { |char| char.ord }
 
-  shifted_values = split_string.map do |char|
-    shifted_value = char.ord + key
-
-    if lowercase_range.cover?(char)
-      shifted_value > 122 ? shifted_value - 26 : shifted_value
-    elsif uppercase_range.cover?(char)
-      shifted_value > 90 ? shifted_value - 26 : shifted_value
+  shifted_values = ascii_values.map do |code|
+    shifted_value = code + key
+    if lowercase.cover?(code)
+      shifted_value > lowercase.end ? shifted_value - 26 : shifted_value
+    elsif uppercase.cover?(code)
+      shifted_value > uppercase.end ? shifted_value - 26 : shifted_value
     else
-      shifted_value = char.ord
+      code
     end
   end
 
-  shifted_values.map { |char| char.chr }.join
+  shifted_values.map { |code| code.chr }.join
 end
 
 puts caesar_cipher("What a string!", 5)
