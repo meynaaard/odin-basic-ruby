c7e1156be4313219b96f94120f9aa67fccaa2f53
meynard <sombritomeynard@gmail.com>
2025-08-12 08:40:04 +0800

    feat: Add nested loop to check for potential sell day

--- 03_stock_picker.rb
+++ 03_stock_picker.rb
@@ -6,11 +6,16 @@ My algorithm:
    `stock_prices` is an array of stock price per day.
 2. Iterate over the `stock_prices` array using #each_with_index, to get the
    index as `day` starting from 0 and the `price` for each day
+3. Nest another iteration to check for potential `sell_day`, skip the day if it
+   is less than or equal the current `day`
 =end
 
 def stock_picker(stock_prices)
   stock_prices.each_with_index do |price, day|
-    puts "Day #{day} - #{price}.00"
+    stock_prices.each_with_index do |sell_price, sell_day|
+      next if sell_day <= day
+      binding.pry
+    end
   end
 end
 
