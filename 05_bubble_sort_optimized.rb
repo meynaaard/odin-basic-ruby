4b695fdef5b5253b7e868204520a872c27c16b1c
meynard <sombritomeynard@gmail.com>
2025-08-15 13:37:53 +0800

    feat: Implement optimized bubble sort algorithm

---
+++ 05_bubble_sort_optimized.rb
@@ -0,0 +1,33 @@
+require "pry-byebug"
+
+def bubble_sort(array)
+  iteration = 1
+
+  loop do
+    is_sorted = true
+
+    (array.length - iteration).times do |i|
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        is_sorted = false
+      end
+    end
+
+    break if is_sorted
+
+    iteration += 1
+  end
+
+  puts
+  array
+end
+
+p bubble_sort([3, 2, 9, 6, 5])
+p bubble_sort([4, 3, 78, 2, 0, 2])
+
+reversed_array = []
+for i in (-128..127).to_a.reverse
+  reversed_array.push(i)
+end
+
+p bubble_sort(reversed_array)
